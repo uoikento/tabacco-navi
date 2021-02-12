bb3c04864bec826bcfa232ae753296e2ea24ade8
kento.uoi <fishperson19@gmail.com>
2021-02-13 01:59:33 +0900

    keyword-serch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
 import React, { useState, useEffect } from 'react'
-import TextField from '@material-ui/core/TextField'
 import './index.css'
 import Shop from './services/shops'
 
 const App = () => {
   const [shops, setShops] = useState([])
-  console.log(shops)
-  useEffect(() => {
-    console.log('effect')
-    Shop.getAll()
-      .then(firstShops => {
-        setShops(firstShops)
+  const [searchWord, setSearchWord] = useState('')
+  // console.log(shops)
+
+  // useEffect(() => {
+  //   console.log('effect')
+  //   Shop.getShops()
+  //     .then(searchShops => {
+  //       setShops(searchShops)
+  //     })
+  // }, [])
+  // const yakiniku = "YAKINIKU MITSUKUNI 焼肉 ミツクニ 光肉"
+  // const shop = shops.filter(s => s.name == yakiniku)
+  // console.log(shop)
+
+  const postWord = (e) => {
+    e.preventDefault()
+    const searchObject = {
+      keyword: searchWord
+    }
+    console.log(searchObject)
+    Shop
+      .getShops(searchObject)
+      .then(searchShops => {
+        setShops(searchShops)
       })
-  }, [])
-  
+      .then(setSearchWord(''))
+  }
+  const handleSearchChange = (event) => {
+    // console.log(event.target.value)
+    setSearchWord(event.target.value)
+  }
   return (
     <div>
       <p>hello</p>
-      <ul>
-        {shops.map(a => a.name)}
-      </ul>
-      <TextField >
-        Hello World
-      </TextField>
+        {(() => {
+          if (shops.length === 0) {
+            return <p>naiyo</p>
+          } else {
+            return <ul>{shops.map(s => s.name)}</ul>
+          }
+        })()}
+        <form onSubmit={postWord}>
+          <input
+            value={searchWord}
+            onChange={handleSearchChange}
+        />
+          <button type="submit">save</button>
+        </form>
     </div>
   )
 }
 
 export default App;
-
