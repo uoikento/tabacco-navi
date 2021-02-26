f8b3aeb82181eb8b8b6a08b63573a46b1bce9fa2
kento.uoi <fishperson19@gmail.com>
2021-02-26 23:54:22 +0900

    genreServise

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
 import Shop from './services/shops'
-import Show from './components/Show';
+import Genre from './services/genres'
+import Header from './components/Header'
+import Show from './components/Show'
+import Select from 'react-select'
 
 const App = () => {
   const [shops, setShops] = useState([])
-  // const [showShops, setShowShop] = useState([])
+  const [genres, setGenres] = useState([])
   const [searchKeyword, setSearchKeyword] = useState('')
   const [searchLat, setSearchLat] = useState('')
   const [searchLng, setSearchLng] = useState('')
+  const [searchGenre, setSearchGenre] = useState('')
+  const [visible, setVisible] = useState(false)
 
+  useEffect(() => {
+    Genre
+      .getGenres()
+      .then(allLocates => {
+        setGenres(allLocates)
+        // console.log(genres)
+    })
+  }, [])
+  
   const postWord = (e) => {
     e.preventDefault()
     const searchObject = {
       keyword: searchKeyword,
+      genre: searchGenre.value,
       lat: searchLat,
       lng: searchLng
     }
@@ -29,55 +44,70 @@ const App = () => {
         }
         // console.log(smoke)
       })
-      .then(setSearchKeyword(''))
+      // .then(setSearchKeyword(''))
   }
-  const handleSearchChange = (event) => {
-    // console.log(event.target.value)
-    setSearchKeyword(event.target.value)
+  const handleSearchKeywordChange = (e) => {
+    setSearchKeyword(e.target.value)
   }
+
   const handleSearchLocation = () => {
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
+    navigator.geolocation.getCurrentPosition(position => {
         console.log(position.coords.latitude)
         console.log(position.coords.longitude)
         const lat = position.coords.latitude
         const lng = position.coords.longitude
         setSearchLat(lat)
         setSearchLng(lng)
+        setVisible(!visible)
       })
   }
   const handleDeleteLocation = () => {
     setSearchLat('')
     setSearchLng('')
+    setVisible(!visible)
   }
-  
-  // const showbody = () => {
-    
-  // }
+  const hideVisible = { display: visible ? 'none': '' }
+  const showVisible = {display: visible ? '': 'none'}
 
   return (
     <div>
-      <p>hello</p>
-      {shops !== null
-        ? (shops.length == 0
-          ? <p>やにやに</p>
-          :<Show shops={shops} />)
-        : <p>該当するお店を見つけることが出来ませんでした。。。</p>
-      }
-        <form onSubmit={postWord}>
-          <input
-            value={searchKeyword}
-          onChange={handleSearchChange}
-          placeholder="キーワード"
-          />
-          <button type="submit">検索</button>
+      <Header />
+      <form onSubmit={postWord}>
+        <input
+          value={searchKeyword}
+        onChange={handleSearchKeywordChange}
+        placeholder="キーワード"
+        />
+        <button type="submit">
+          keyword:（{searchKeyword}）
+          {/* genre: （{searchGenre.label}） */}
+          location:{searchLat== ''
+            ? "地名"
+            : "現在地"
+          }
+          <br />
+          検索
+        </button>
       </form>
-      <button id='getLocation' onClick={handleSearchLocation}>
-        位置情報取得
-      </button>
-      <button onClick={handleDeleteLocation}>
-        位置情報削除
-      </button>
+      <div style={hideVisible}>
+        <button id='getLocation' onClick={handleSearchLocation}>
+          位置情報取得
+        </button>
+      </div>
+      <div style={showVisible}>
+        <button onClick={handleDeleteLocation}>
+          位置情報削除
+        </button>
+      </div>
+      <Select options={genres} defaultValue={searchGenre} onChange={setSearchGenre} isClearable={true}/>
+      <div>
+        {shops !== null
+          ? (shops.length == 0
+            ? <p>ヤニ切れ</p>
+            :<Show shops={shops} />)
+          : <p>該当するお店を見つけることが出来ませんでした。。。</p>
+        }
+      </div>
     </div>
   )
 }
--- src/services/shops.js
+++ src/services/shops.js
@@ -1,12 +1,12 @@
 import axios from "axios"
-const baseUrl = 'http://localhost:3001/api'
+const baseUrl = 'http://localhost:3001/api/shops'
 
 const getShops = (keyword) => {
   const getRequest = axios.post(baseUrl, keyword )
   return (
     getRequest.then(response => {
       const data = response.data
-      console.log(data)
+      // console.log(data)
       return data
     })
   )
