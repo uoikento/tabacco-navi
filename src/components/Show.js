7e351e11bc2e22c30621c577d9a7638abf884b58
kento.uoi <fishperson19@gmail.com>
2021-02-26 23:53:41 +0900

    showChildComponent

--- src/components/Show.js
+++ src/components/Show.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 const Show = (props) => {
   const shop = props.shops
-  console.log(shop)
-  console.log(props)
+  // console.log(shop)
+  // console.log(props)
   return (
     <div>
       <ul>
-        {shop.map(shop => <li>{shop.name}</li>)}
+        {shop.map((shop, index) =>
+          <li key={index}>
+            {index}{shop.name}
+            <img src={shop.photo.pc.s} />
+            <a href={shop.urls.pc} target="_balnk">hotpeperで開く</a>
+          </li>
+        )}
       </ul>
     </div>
   )
